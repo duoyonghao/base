82d0ed81c2c4eeeaf2549546526faad5867a5d8f
bojinhong <bojinhong@126.com>
2020-06-13 17:24:39 +0800

    gitlab-ci脚本编写

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,8 @@
 echo "===========开始部署============="
 echo "===========停止服务器==============="
 
-cd D:\tomcat\apache-tomcat-8.0.36\bin && .\shutdown.bat
-rm -rf D:\tomcat\apache-tomcat-8.0.36\webapps\base
-cp -r target\base D:\tomcat\apache-tomcat-8.0.36\webapps
-cd D:\tomcat\apache-tomcat-8.0.36\bin && .\startup.bat
+cd %1%\bin && .\shutdown.bat
+rm -rf %1%\webapps\base
+cp -r target\base %1%\webapps
+cd %1%\bin && .\startup.bat
 echo "================部署结束============"
