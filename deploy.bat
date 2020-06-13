a6005e235d7fe08492e8b1f70a7179e34141be60
bojinhong <bojinhong@126.com>
2020-06-13 17:55:42 +0800

    gitlab-ci脚本编写

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,4 @@
-@echo off
-rem starting deploy
-cd %1%\bin && .\shutdown.bat > nul 2>&1
+try { cd %1%\bin && .\shutdown.bat } catch {}
 rm -rf %1%\webapps\base
 cp -r target\base %1%\webapps
 cd %1%\bin && .\startup.bat
-rem deploy completed
