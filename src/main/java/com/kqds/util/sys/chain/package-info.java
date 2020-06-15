89e01a88de05284d2f8bf4ac56a82699afa94202
bojinhong <bojinhong@126.com>
2020-06-15 14:37:42 +0800

    保持线上版本一致

--- src/main/java/com/kqds/util/sys/chain/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-/**
- * @author Administrator
- *
- */
-package com.kqds.util.sys.chain;
--- src/main/java/com/kqds/util/sys/connection/DataSourceInstances.java
+++
@@ -1,6 +0,0 @@
-package com.kqds.util.sys.connection;
-
-public class DataSourceInstances {
-	public static final String KQDS = "KQDS";
-	public static final String KQDSKP = "KQDSKP";
-}
