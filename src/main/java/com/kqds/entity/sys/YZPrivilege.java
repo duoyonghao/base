dd6341ef538c18e96da51bf808c5067790c8ef63
bojinhong <bojinhong@126.com>
2020-07-10 17:03:05 +0800

    创建权限同步器

--- src/main/java/com/kqds/entity/sys/YZPrivilege.java
+++ src/main/java/com/kqds/entity/sys/YZPrivilege.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class YZPrivilege {
     private Long id;
-    private String belongs_to;
-    private String user_seq_id;
-    private String belongs_to_name;
-    private String user_name;
+    private String belongsTo;
+    private String userSeqId;
+    private String belongsToName;
+    private String userName;
 }
