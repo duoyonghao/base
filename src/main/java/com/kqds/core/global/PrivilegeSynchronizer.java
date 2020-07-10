eee2408cb2eb0d9d0ec17bdd6994ad7711c55297
bojinhong <bojinhong@126.com>
2020-07-10 16:51:35 +0800

    创建权限同步器

--- src/main/java/com/kqds/core/global/PrivilegeSynchronizer.java
+++ src/main/java/com/kqds/core/global/PrivilegeSynchronizer.java
@@ -1,14 +1,18 @@
 package com.kqds.core.global;
 
 import com.kqds.entity.sys.YZPerson;
+import com.kqds.entity.sys.YZPriv;
+import com.kqds.entity.sys.YZPrivilege;
 import com.kqds.service.sys.person.YZPersonLogic;
 import com.kqds.service.sys.priv.YZPrivLogic;
 import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.util.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collectors;
@@ -26,16 +30,49 @@ public class PrivilegeSynchronizer implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         new Thread(() -> {
-            String seqId;
             while (true) {
                 try {
-                    seqId = waitingUsers.take();
+                    String seqId = waitingUsers.take();
                     if(StringUtils.isNotEmpty(seqId)) {
                         //执行同步操作
-                        logger.info("PrivilegeSynchronizer: can not find user [{}] privileges", seqId);
+                        logger.info("PrivilegeSynchronizer: user [{}] privileges changed", seqId);
+                        //全部删除当前用户的权限，并重新创建
+                        try {
+                            yzPrivLogic.removeUserPrivileges(seqId);
+                            YZPerson yzPerson = yzPersonLogic.getPersonBySeqId(seqId);
+                            if(yzPerson != null) {
+                                String privSeqId = yzPerson.getUserPriv();
+                                YZPriv yzPriv = yzPrivLogic.getDetailBySeqId(privSeqId);
+                                String visualPerson = yzPriv.getVisualPerson();
+                                if(StringUtils.isNotEmpty(visualPerson)) {
+                                    List<String> userSeqIds = Arrays.asList(visualPerson.split(","));
+                                    userSeqIds.forEach(userSeqId->{
+                                        YZPrivilege yzPrivilege = new YZPrivilege();
+                                        yzPrivilege.setBelongs_to(seqId);
+                                        yzPrivilege.setBelongs_to_name(yzPerson.getUserName());
+                                        yzPrivilege.setUser_seq_id(userSeqId);
+                                        YZPerson toUser = null;
+                                        try {
+                                            toUser = yzPersonLogic.getPersonBySeqId(userSeqId);
+                                            yzPrivilege.setUser_name(toUser.getUserName());
+                                        } catch (Exception e) {
+                                            logger.error(e.getMessage());
+                                        }
+                                        try {
+                                            yzPrivLogic.insertPrivilege(yzPrivilege);
+                                        } catch (Exception e) {
+                                            logger.error(e.getMessage());
+                                        }
+                                    });
+                                }
+                            }
+                        } catch (Exception e) {
+                            logger.error(e.getMessage());
+                        }
+
                     }
                 } catch (InterruptedException e) {
-                    logger.debug(e.getMessage());
+                    logger.error(e.getMessage());
                 }
 
             }
@@ -55,11 +92,11 @@ public class PrivilegeSynchronizer implements InitializingBean {
                                 waitingUsers.add(seqId);
                             }
                         } catch (Exception e) {
-                            logger.debug(e.getMessage());
+                            logger.error(e.getMessage());
                         }
                     });
                 } catch (Exception e) {
-                    logger.debug(e.getMessage());
+                    logger.error(e.getMessage());
                 }
             }
         }.start();
